88ee5b90a47ba5ec79b797b947a5f4af3f2cd11a
lightzane <bhest.pat@gmail.com>
2021-04-15 13:41:33 +0800

    added GET req.params

--- server/routes/api.js
+++ server/routes/api.js
@@ -12,7 +12,7 @@ router.get('/', (req,res) => res.json(sample) )
 // GET /api/fibonacci
 router.get('/fibonacci', (req,res) => res.json(fibonacci) )
 
-// POST /api/calculate-sequence
+// POST /api/post-calculate-sequence
 router.post('/post-calculate-sequence', (req,res) => {
     let data = req.body
     console.log('POST ------------------')
@@ -26,11 +26,27 @@ router.post('/post-calculate-sequence', (req,res) => {
     res.json({ "sequence": sequence })
 })
 
+// GET /api/get-calculate-sequence
+// ?x=1&y=3
 router.get('/get-calculate-sequence', (req,res) => {
     let data = req.query
     console.log('GET ------------------')
     console.log(data)
     
+    let sequence = Fibo.getSequence({
+        x: parseFloat(data.x),
+        y: parseFloat(data.y)
+    })
+
+    res.json({ "sequence": sequence })
+})
+
+// GET /api/get-calculate-sequence/x/:x/y/:y
+router.get('/get-calculate-sequence/x/:x/y/:y', (req,res) => {
+    let data = req.params
+    console.log('GET II ------------------')
+    console.log(data)
+
     let sequence = Fibo.getSequence({
         x: parseFloat(data.x),
         y: parseFloat(data.y)
